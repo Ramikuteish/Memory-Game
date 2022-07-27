9551b33cbc18cc24c1ec55f12f4bc89e46d08fbd
Rami <rami.ktish@gmail.com>
2022-07-27 18:29:04 +0200

    images added

--- app/src/Views/GameBoard.js
+++ app/src/Views/GameBoard.js
@@ -5,18 +5,20 @@ import { useState } from "react";
 
 function GameBoard() {
   const cards = [
-    "https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg",
-    "https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg",
-    "https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg",
-    "https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg",
-    "https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg",
-    "https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg",
-    "https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg",
-    "https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg",
-    "https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg",
-    "https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg",
-    "https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg",
-    "https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg",
+    "https://www.memozor.com/jeux/jquery/emoji_1/image7.png",
+    "https://www.memozor.com/jeux/jquery/emoji_1/image10.png",
+
+    "https://i.pinimg.com/564x/c1/dc/42/c1dc42a15bb6611410acedc8a0716a1a--happy-smiley-face-smiley-faces.jpg",
+
+    "https://www.memozor.com/jeux/jquery/emoji_1/image7.png",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh1VNE4wucEZZNUqEycTxGdTJthyJ_IOhHJnafJtJjLVLWLO6Ko9UQnRN0W6WbzgNCXb4&usqp=CAU",
+    "https://as1.ftcdn.net/v2/jpg/02/15/08/80/1000_F_215088044_Ow0pypSekAamu3jZJnkRtfAyKj6KVlKj.jpg",
+    "https://i.pinimg.com/564x/c1/dc/42/c1dc42a15bb6611410acedc8a0716a1a--happy-smiley-face-smiley-faces.jpg",
+    "https://pbs.twimg.com/media/CxjE1eOUoAAwBbk.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh1VNE4wucEZZNUqEycTxGdTJthyJ_IOhHJnafJtJjLVLWLO6Ko9UQnRN0W6WbzgNCXb4&usqp=CAU",
+    "https://www.memozor.com/jeux/jquery/emoji_1/image10.png",
+    "https://pbs.twimg.com/media/CxjE1eOUoAAwBbk.jpg",
+    "https://as1.ftcdn.net/v2/jpg/02/15/08/80/1000_F_215088044_Ow0pypSekAamu3jZJnkRtfAyKj6KVlKj.jpg",
   ];
 
   const [clicks, setClicks] = useState(0);
